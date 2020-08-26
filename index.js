6acdad654f44e5d4277e864992c4a7d7538ede34
beejoybee <68606120+beejoybee@users.noreply.github.com>
2020-08-26 11:08:06 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+document.querySelector("button").onclick = function handleClick(event) {
+
+ var loveScore = Math.floor(Math.random() * 100) + 1;
+
+ if (loveScore >= 70) {
+   var perfectScore = document.querySelector("h3").innerHTML =  "Your love score is " + loveScore + ", you are a perfect pair!";
+   var heart1 = document.querySelector("img").setAttribute("src", "images/heart1.png");
+   return perfectScore, heart1;
+ } else if (loveScore < 70 && loveScore > 50) {
+   var mediumScore = document.querySelector("h3").innerHTML = "Your love score is " + loveScore + ", you'll need to work to make it work.";
+   var heart2 = document.querySelector("img").setAttribute("src", "images/heart2.png");
+   return mediumScore, heart2;
+ } else {
+   var badScore = document.querySelector("h3").innerHTML = "Your love score is " + loveScore + ", save yourself the heartache.";
+   var heart3 = document.querySelector("img").setAttribute("src", "images/heart3.png");
+   return badScore, heart3;
+ }
+ }
